52e0526eacf6ae51b5de0cfefea96d063b9cce1a
andreicostandache <andrei97mihai@gmail.com>
2021-07-13 21:38:07 +0300

    Add files via upload

---
+++ part 1/Laborator3/Business.Test/ITest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Test
+{
+    interface ITest
+    {
+        void When_RetrieveAllBooksIsCalled_Then_ShouldReturnEnumerableOfBooks();
+        void When_RetrieveAllOrderByYearDescending_Then_ShouldReturnEnumerableOfBooksOrderedByYearDescending();
+        void When_RetrieveAllOrderByYearAscending_Then_ShouldReturnEnumerableOfBooksOrderedByYearAscending();
+        void When_RetrieveAllOrderByPriceDescending_Then_ShouldReturnEnumerableOfBooksOrderedByPriceDescending();
+        void When_RetrieveAllOrderByPriceAscending_Then_ShouldReturnEnumerableOfBooksOrderedByPriceAscending();
+        void When_RetrieveAllBooksGroupByGenreIsCalled_Then_ShouldReturnGroupsOfGenres();
+
+    }
+}
